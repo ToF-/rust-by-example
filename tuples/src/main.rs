85bc4a74478fc7da6385f610903097c5a8a8ca06
Christophe Thibaut <cthibaut@octo.com>
2023-07-20 22:15:19 +0200

    tuples activity

--- tuples/src/main.rs
+++ tuples/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 // Tuples can be used as function arguments and as return values.
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
     // `let` can be used to bind the members of a tuple to variables.
@@ -10,6 +12,15 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
 
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})\n({}, {})", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn transpose(matrix: Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
 fn main() {
     // A tuple with a bunch of different types.
     let long_tuple = (1u8, 2u16, 3u32, 4u64,
@@ -50,4 +61,8 @@ fn main() {
 
     let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
     println!("{:?}", matrix);
+    println!("{}", matrix);
+
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
 }
