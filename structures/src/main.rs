c4d5e90fa3c7c64621bae7b640d7dcfeea3deec7
Christophe Thibaut <cthibaut@octo.com>
2023-07-21 00:46:02 +0200

    structures

---
+++ structures/src/main.rs
@@ -0,0 +1,87 @@
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+// A unit struct
+struct Unit;
+
+// A tuple struct
+struct Pair(i32, f32);
+
+// A struct with two fields
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// Structs can be reused as fields of another struct
+struct Rectangle {
+    // A rectangle can be specified by where the top left and bottom right
+    // corners are in space.
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn rect_area(rectangle : Rectangle) -> f32 {
+    let Rectangle { top_left: Point { x: x0, y: y0 }, bottom_right: Point { x : x1, y: y1 } } = rectangle;
+    f32::abs(x0 - x1) * f32::abs(y0 - y1)
+}
+
+fn square(point: Point, length: f32) -> Rectangle {
+    let Point { x: x0, y: y0 } = point;
+    Rectangle { top_left: point, bottom_right: Point { x: x0 + length, y: y0 + length } }
+}
+fn main() {
+    // Create struct with field init shorthand
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Print debug struct
+    println!("{:?}", peter);
+
+    // Instantiate a `Point`
+    let point: Point = Point { x: 10.3, y: 0.4 };
+
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    // Make a new point by using struct update syntax to use the fields of our
+    // other one
+    let bottom_right = Point { x: 5.2, ..point };
+
+    // `bottom_right.y` will be the same as `point.y` because we used that field
+    // from `point`
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    // Destructure the point using a `let` binding
+    let Point { x: left_edge, y: top_edge } = point;
+
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    };
+    println!("rectangle area: {}", rect_area(Rectangle { top_left: Point { x:0.0, y:0.0 }, bottom_right: Point { x:5.0, y:3.0}}));
+
+    // Instantiate a unit struct
+    let _unit = Unit;
+
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
+
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
+}
+
